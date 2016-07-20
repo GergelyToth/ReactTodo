5e26e0c1e8985aa926038f0c946fbb4a0316567c
Gergely Toth <tgergely18@gmail.com>
2016-07-20 19:40:09 +0100

    add filter functionality

--- app/api/TodoAPI.jsx
+++ app/api/TodoAPI.jsx
@@ -18,5 +18,31 @@ module.exports = {
 		}
 
 		return $.isArray(todos) ? todos : [];
+	},
+	filterTodos: function(todos, showCompleted, searchText) {
+		var filteredTodos = todos;
+
+		// Filtered by showCompleted
+		filteredTodos = filteredTodos.filter((todo) => !todo.completed || showCompleted);
+
+		// Filter by searchText
+		filteredTodos = filteredTodos.filter((todo) => {
+			var text = todo.text.toLowerCase();
+
+			return !searchText || text.indexOf(searchText.toLowerCase()) > -1;
+		});
+
+		// Sort todos with non-completed first
+		filteredTodos.sort((a, b) => {
+			if (!a.completed && b.completed) {
+				return -1;
+			} else if (a.completed && !b.completed) {
+				return 1;
+			} else {
+				return 0;
+			}
+		});
+
+		return filteredTodos;
 	}
 };
