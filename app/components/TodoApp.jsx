b65fa1f7752f5ef5b6225bd9edd4df61e36072e2
Gergely Toth <tgergely18@gmail.com>
2016-07-28 23:38:29 +0100

    refactor TodoList, Todo and AddTodo to work with redux

--- app/components/TodoApp.jsx
+++ app/components/TodoApp.jsx
@@ -2,8 +2,8 @@ var React = require('react');
 var uuid = require('node-uuid');
 var moment = require('moment');
 
-var TodoList = require('TodoList');
-var AddTodo = require('AddTodo');
+import TodoList from 'TodoList';
+import AddTodo from 'AddTodo';
 var TodoSearch = require('TodoSearch');
 var TodoAPI = require('API/TodoAPI');
 
@@ -38,23 +38,7 @@ var TodoApp = React.createClass({
 			searchText: searchText.toLowerCase()
 		});
 	},
-	handleToggle: function(id) {
-		var updatedTodos = this.state.todos.map((todo) => {
-			if (todo.id === id) {
-				todo.completed = !todo.completed;
-				todo.completedAt = todo.completed ? moment().unix() : undefined;
-			}
-			return todo;
-		});
-
-		this.setState({
-			todos: updatedTodos
-		});
-	},
 	render: function() {
-		var {todos, showCompleted, searchText} = this.state;
-		var filteredTodos = TodoAPI.filterTodos(todos, showCompleted, searchText);
-		
 		return (
 			<div>
 				<h1 className="page-title">Todo App</h1>
@@ -63,7 +47,7 @@ var TodoApp = React.createClass({
 					<div className="column small-centered small-11 medium-6 large-5">
 						<div className="container">
 							<TodoSearch onSearch={this.handleSearch}/>
-							<TodoList todos={filteredTodos} onToggle={this.handleToggle}/>
+							<TodoList/>
 							<AddTodo onSetTodo={this.handleAddTodo}/>
 						</div>
 					</div>
--- app/tests/components/AddTodo.test.jsx
+++ app/tests/components/AddTodo.test.jsx
@@ -4,33 +4,38 @@ var TestUtils = require('react-addons-test-utils');
 var $ = require('jQuery');
 var expect = require('expect');
 
-var AddTodo = require('AddTodo');
+var {AddTodo} = require('AddTodo');
 
 describe('AddTodo', () => {
 	it('should exist', () => {
 		expect(AddTodo).toExist();
 	});
 
-	it('should call onSetTodo if text entered', () => {
+	it('should dispatch ADD_TODO when valid todo text', () => {
 		var todoText = 'test todo';
+		var action = {
+			type: 'ADD_TODO',
+			text: todoText
+		};
 		var spy = expect.createSpy();
-		var addTodo = TestUtils.renderIntoDocument(<AddTodo onSetTodo={spy}/>);
+		var addTodo = TestUtils.renderIntoDocument(<AddTodo dispatch={spy}/>);
 		var $el = $(ReactDOM.findDOMNode(addTodo));
 
 		addTodo.refs.todoText.value = todoText;
 		TestUtils.Simulate.submit($el.find('form')[0]);
 
-		expect(spy).toHaveBeenCalledWith(todoText);
+		expect(spy).toHaveBeenCalledWith(action);
 	});
 
-	it('should not call onSetTodo if no text entered', () => {
+	it('should not dispatch ADD_TODO when invalid text', () => {
+		var todoText = '';
 		var spy = expect.createSpy();
-		var addTodo = TestUtils.renderIntoDocument(<AddTodo onSetTodo={spy}/>);
+		var addTodo = TestUtils.renderIntoDocument(<AddTodo dispatch={spy}/>);
 		var $el = $(ReactDOM.findDOMNode(addTodo));
 
-		addTodo.refs.todoText.value = '';
+		addTodo.refs.todoText.value = todoText;
 		TestUtils.Simulate.submit($el.find('form')[0]);
 
 		expect(spy).toNotHaveBeenCalled();
-	})
+	});
 });
