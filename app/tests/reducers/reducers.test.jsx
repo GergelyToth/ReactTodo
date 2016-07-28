aba4cc4823d2d840e77f3c833b951497f4bb70aa
Gergely Toth <tgergely18@gmail.com>
2016-07-28 21:43:48 +0100

    add todosReducer and tests

--- app/tests/reducers/reducers.test.jsx
+++ app/tests/reducers/reducers.test.jsx
@@ -25,4 +25,53 @@ describe('Reducers', () => {
 			expect(res).toEqual(true);
 		});
 	});
+
+	describe('todosReducer', () => {
+		it('should add new todo', () => {
+			var action = {
+				type: 'ADD_TODO',
+				text: 'Walk the dog'
+			};
+			var res = reducers.todosReducer(df([]), df(action));
+
+			expect(res.length).toEqual(1);
+			expect(res[0].text).toEqual(action.text);
+		});
+
+		it('should toggle todo to true and set completedAt', () => {
+			var todos = [{
+				id: 1,
+				text: 'Walk the dog',
+				completed: false,
+				createdAt: 1234,
+				completedAt: undefined
+			}];
+			var action = {
+				type: 'TOGGLE_TODO',
+				id: 1
+			};
+			var res = reducers.todosReducer(df(todos), df(action));
+
+			expect(res[0].completed).toBe(true);
+			expect(res[0].completedAt).toBeA('number');
+		});
+
+		it('should toggle todo to false and remove completedAt', () => {
+			var todos = [{
+				id: 1,
+				text: 'Walk the dog',
+				completed: true,
+				createdAt: 1234,
+				completedAt: 12345
+			}];
+			var action = {
+				type: 'TOGGLE_TODO',
+				id: 1
+			};
+			var res = reducers.todosReducer(df(todos), df(action));
+
+			expect(res[0].completed).toBe(false);
+			expect(res[0].completedAt).toBe(undefined);
+		});
+	});
 });
